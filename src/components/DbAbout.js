0ec27471b8babe09d5c9c22209fa78c0cb6f7e00
Dhananjaya Swain <85564771+thelastone07@users.noreply.github.com>
2021-10-19 23:49:47 +0530

    Update DbAbout.js

--- src/components/DbAbout.js
+++ src/components/DbAbout.js
@@ -6,13 +6,11 @@ export const DbAbout=() => {
       <div className="">
         <div className="">
             <h1 className="">About this Event</h1>
-            <h3>
-              A corny line to motivate peeps
-            </h3>
+        
         </div>
         <div>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia animi error possimus reiciendis assumenda in repellat harum necessitatibus, soluta, tenetur et! Aperiam, rerum possimus ratione atque reprehenderit explicabo ipsum recusandae libero quidem minus ad, consequuntur, repellendus sed. Perferendis beatae explicabo vero nisi possimus animi excepturi dolor nulla soluta dolore, facilis ut repellendus obcaecati fuga quidem. Repellendus a exercitationem delectus minus!
+            ipsum dolor sit amet consectetur adipisicing elit. Officia animi error possimus reiciendis assumenda in repellat harum necessitatibus, soluta, tenetur et! Aperiam, rerum possimus ratione atque reprehenderit explicabo ipsum recusandae libero quidem minus ad, consequuntur, repellendus sed. Perferendis beatae explicabo vero nisi possimus animi excepturi dolor nulla soluta dolore, facilis ut repellendus obcaecati fuga quidem. Repellendus a exercitationem delectus minus!
           </p>
         </div>
       </div>
