f439833f4134b4da603065c75dfbad9adf147f8a
Dhananjaya Swain <85564771+thelastone07@users.noreply.github.com>
2021-10-19 23:26:09 +0530

    Add files via upload

---
+++ src/components/Splash.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./splash.css"
+
+export const Splash =()=>{
+    return (
+        <>
+          <div class="db-welcome">
+      <span id="db-splash-overlay" class="db-splash"></span>
+      <span id="db-welcome" class="db-z-depth-4"></span>
+    
+    </div>
+        </>
+      );
+}
