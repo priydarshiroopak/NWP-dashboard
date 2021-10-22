3e51f5a6bc3fb8cfd166ae182750613aeb6fece1
Roopak <priydarshiroopak@gmail.com>
2021-10-22 17:53:48 +0530

    Updates

--- dashboard.js
+++ dashboard.js
@@ -45,4 +45,45 @@ window.addEventListener('load', () => {
     offsetMenuBorder(activeItem, menuBorder);
     menu.style.setProperty("--timeOut", "none");
     });
+
+    const headingTl = gsap.timeline({
+        repeat: 0
+      });
+      
+      headingTl
+        .from(".db-heading", {
+          duration: 1,
+          scaleX: 0,
+          transformOrigin: "left",
+          ease: "expo.inOut"
+        })
+        .from(
+          ".db-heading h1",
+          {
+            y: "100%",
+            duration: 0.8,
+            ease: "expo.out"
+          },
+          "-=0.2"
+        )
+        .from(
+          ".db-heading",
+          {
+            css: { borderBottom: "0.2rem solid black" },
+            duration: 2,
+            transformOrigin: "right",
+            ease: "none"
+          },
+          "-=1"
+        )
+        .from(
+          ".db-heading h1",
+          {
+            duration: 2,
+            transformOrigin: "right",
+            ease: "none",
+            css: { color: "black" }
+          },
+          "-=2"
+        );
 });
