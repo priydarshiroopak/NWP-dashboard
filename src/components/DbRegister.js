21300573c40aa5212004d06f74bb3a13b1471605
Dhananjaya Swain <85564771+thelastone07@users.noreply.github.com>
2021-10-22 22:23:40 +0530

    Update DbRegister.js

--- src/components/DbRegister.js
+++ src/components/DbRegister.js
@@ -4,10 +4,10 @@ export const DbRegister =() => {
   return (
     <>
        <div className="db-container">
-        <div className="db-flex-items db-heading">
-            <h1 className=""
+        <div className="db-flex-items">
+          <div className="db-heading">  <h1 className=""
             style={{backgroundColor:"#ffe797"}} >Register here!</h1>
-        
+        </div>
         <div className="db-content">
           <h3>--this section is for offline submissions--</h3>
           </div>
