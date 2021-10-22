136d2d9c3e3824766f0039ba98d82a0df64140a3
Dhananjaya Swain <85564771+thelastone07@users.noreply.github.com>
2021-10-22 20:03:48 +0530

    Add files via upload

---
+++ src/components/DbRules.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+export const DbRules = () => {
+  return (
+    <>
+      <div className="db-container">
+        <div className="db-flex-items">
+         
+            <h1 style={{backgroundColor:'#ff96bd'}} className="db-heading">important!</h1>
+             
+          <div className="db-content">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Perferendis dolore laudantium ea eos mollitia necessitatibus!
+            </p>
+            
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Perferendis dolore laudantium ea eos mollitia necessitatibus!
+            </p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Perferendis dolore laudantium ea eos mollitia necessitatibus!
+            </p><p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Perferendis dolore laudantium ea eos mollitia necessitatibus!
+            </p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Perferendis dolore laudantium ea eos mollitia necessitatibus!
+            </p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Perferendis dolore laudantium ea eos mollitia necessitatibus!
+            </p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Perferendis dolore laudantium ea eos mollitia necessitatibus!
+            </p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Perferendis dolore laudantium ea eos mollitia necessitatibus!
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
